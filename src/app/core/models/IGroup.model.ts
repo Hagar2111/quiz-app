e110db85e3b00562fee9cd64bb58a050e7f48432
yasmingamil22 <yasmingamy@gmail.com>
2024-06-27 19:09:01 +0300

    msg

--- src/app/core/models/IGroup.model.ts
+++ src/app/core/models/IGroup.model.ts
@@ -7,6 +7,33 @@ export interface IGroup {
   max_students: number
 }
 
+
+export interface IGroupResponse {
+  _id: string
+name: string
+status: string
+instructor: string
+students: string[]
+max_students: number
+}
+
+export interface IStudent {
+  _id: string
+  first_name: string
+  last_name: string
+  email: string
+}
+
+export interface IUpdateOrAddGroup{
+  name: string
+  students: string[]
+}
+
+export interface IAddUpdatedDeleteGroupRes {
+  data: IGroupResponse
+  message: string
+}
+
 export interface Student {
     _id: string
     first_name: string
--- src/app/features/instructor/dashboard/modules/groups/groups.module.ts
+++ src/app/features/instructor/dashboard/modules/groups/groups.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { GroupsRoutingModule } from './groups-routing.module';
 import { GroupsComponent } from './groups.component';
+import { AddEditViewGroupComponent } from './compontents/add-edit-view-group/add-edit-view-group.component';
 
 
 @NgModule({
   declarations: [
-    GroupsComponent
+    GroupsComponent,
+    AddEditViewGroupComponent
   ],
   imports: [
     CommonModule,
